34a93a49d1044a29c4f16b8454fb3c14196cf3ad
Dev-Lucas-Gabriel <dev.lucasgabriel1@gmail.com>
2026-04-09 22:01:48 -0300

    lista de questões

---
+++ DOM/script.js
@@ -0,0 +1,9 @@
+//console.log(document)
+
+//const guest = document.getElementsByClassName(".guest")
+//const guest = document.querySelector("#guest-2")
+guest.textContent = "Edu"
+
+console.log(guest.textContent)
+
+
---
+++ listaquestoes08.04/script.js
@@ -0,0 +1,127 @@
+// QUESTÃO 1
+const user = {
+    email: "dev.lucasgabriel1@gmail.com",
+    nome: "Lucas Gabriel",
+    idade: 19,
+// QUESTÃO 2
+    message: function() {
+        console.log(`olá meu nome é ${user.nome}`)
+    }
+}
+user.message()
+
+// QUESTÃO 3
+user.email = "dev.lucasgabriel2@gmail.com"
+console.log(user.email)
+
+// QUESTÃO 4
+console.log(user["idade"])
+
+// QUESTÃO 5
+const address = {
+    endereco: {
+        cidade: "Amontada"
+    },   
+}
+
+console.log(address.endereco.cidade)
+
+// QUESTÃO 6
+
+const obj = {
+    city: "Itapipoca",
+    // name: "Judas",
+    id: "12345",
+
+    mensagem : function () {
+        console.log(this?.name)
+    }
+}
+
+obj.mensagem()
+
+// QUESTÃO 7
+let content = {
+    locate: "amontada",
+    idd: null
+}
+console.log(content.idd ?? "default")
+
+// QUESTÃO 8
+let frase = "estou estudando javascript intermediario"
+console.log(frase.toUpperCase)
+console.log(frase.toLowerCase)
+
+// QUESTÃO 9
+let password = "23421"
+
+if (password.length < 6) {
+    console.log("Sua senha deve conter no minimo 6 caracteres")
+}
+
+// QUESTÃO 10 
+console.log(frase.replace("estudando" , "aprendendo"))
+
+// QUESTÃO 11
+console.log(frase.slice(0, 5))
+
+// QUESTÃO 12
+let spaces = "          judas           "
+console.log(spaces.trim)
+
+// QUESTÃO 13
+let fullName = "Judas Escariote"
+console.log(fullName.split(" "))
+
+// QUESTÃO 14
+const palavras = ["olá", "mundo", "javascript"]
+const resultado = palavras.join("-")
+
+console.log(resultado)
+
+// QUESTÃO 15
+const texto = "Judas Escariote"
+const palavra = "escariote"
+const existe = texto.toLowerCase().includes(palavra.toLowerCase())
+console.log(existe)
+
+// QUESTÃO 16
+let exmp = ["banana", "maçã", "manga"]
+console.log(exmp.length)
+
+// QUESTÃO 17
+let nums = ["10", "20", "30"]
+console.log(nums[0])
+console.log(nums[nums.length - 1])
+
+// QUESTÃO 18
+const numbers = [2, 3, 4]
+
+numbers.unshift(1)
+numbers.push(5)
+
+console.log(numbers)
+
+// QUESTÃO 19
+numbers.pop()
+numbers.shift()
+
+console.log(numbers)
+
+// QUESTÃO 20
+let letras = ["a", "b", "c"]
+console.log(letras.indexOf("a"))
+
+// QUESTÃO 21
+console.log(letras.splice(2,2))
+
+// QUESTÃO 23
+let capitals = ["Fortal", "SP", "DF"]
+console.log(capitals.includes("Fortal"))
+
+// QUESTÃO 24
+
+for (i = 0; i <= 10; i++) {
+    console.log(i)
+}
+
